763594abaad10250503535007e0d9f53f602bcb6
lilJuan4168 <chesteraraoz12.0@gmail.com>
2023-10-02 23:36:02 -0300

    update

--- front/pages/3_Search.py
+++ front/pages/3_Search.py
@@ -1,17 +1,24 @@
 import streamlit as st
 import urllib3
 from functions.search_products import search_products
+import json
 
 with st.form(key='my_search'):
         st.write("<h3>Find what you are looking for!</h3>", unsafe_allow_html=True)
         item = st.text_input('', placeholder="iphone 14 pro max")
         submitted = st.form_submit_button('Search')
         if submitted:
-            results = search_products("APP_USR-3750359463586153-093013-74f30a003501175650feeb53496be82f-142830670", "item")
-            st.write("complete") 
+            #results = search_products("APP_USR-3750359463586153-093013-74f30a003501175650feeb53496be82f-142830670", "item")
+            st.success("complete") 
 
-total = results['paging']['total']
-per_tag = total // 4
+#total = results['paging']['total']
+#per_tag = total // 4
+
+with open("front/pages/test.json", 'r') as js:
+     data = json.load(js)
+
+total = data['paging']['total']    
+
 
 if submitted:
     st.write("Total items:", total)
@@ -19,9 +26,13 @@ if submitted:
     pag1, pag2, pag3, pag4, pag5, pag6 = st.tabs(["pag1", "pag2", "pag3", "pag4", "pag5", "pag6"])
 
     with pag1:
-        cnt1, cnt2, cnt3 = st.container()
-        with cnt1:
-            item1= results['results'][0]
+        st.write(data['results'][0]['title'])
+        st.write(data['results'][0]['price'])
+        st.image(data['results'][0]['thumbnail'])
+
+        st.divider()
+
+        st.write(data['results'][1]['title'])
+        st.write(data['results'][1]['price'])
+        st.image(data['results'][1]['thumbnail'])
             
-    with pag2:
-        st.write("hola1")
